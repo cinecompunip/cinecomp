1c937a94041aaada292a30718d6176f0b27d0d2c
VitorFantin <fantinfx@gmail.com>
2022-04-13 21:46:19 -0300

    escrevendo historia 1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-function ativaNoScroll() {
+// function ativaNoScroll() {
 	
-	document.querySelectorAll('img').forEach((img) => {
-		if(img.getBoundingClientRect().top < window.innerHeight) {
-			img.src = img.getAttribute('data-src');
-		};		
-	})
-}
-window.addEventListener('scroll', ativaNoScroll);
+// 	document.querySelectorAll('img').forEach((img) => {
+// 		if(img.getBoundingClientRect().top < window.innerHeight) {
+// 			img.src = img.getAttribute('data-src');
+// 		};		
+// 	})
+// }
+// window.addEventListener('scroll', ativaNoScroll);
